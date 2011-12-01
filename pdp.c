82fd95edcb1379e75aa9af8bb1dc84d4bfd7c5ee
Alexey Lapitsky <lex@realisticgroup.com>
2011-12-01 19:12:29 +0100

    add size for some extended headers + imsi_to_int func

--- pdp.c
+++ pdp.c
@@ -12,19 +12,18 @@
 
 struct header_len gtp_headers[] = {
     {GTP_EXT_IMSI, 8},
-    {GTP_EXT_RAI, 0},
+    {GTP_EXT_RAI, 6},
     {GTP_EXT_RECOVER, 1},
     {GTP_EXT_SEL_MODE, 1},
     {GTP_EXT_TEID, 4},
     {GTP_EXT_TEID_CP, 4},
     {GTP_EXT_NSAPI, 1},
     {GTP_EXT_CHRG_CHAR, 2},
-    {GTP_EXT_TRACE_REF, 0},
-    {GTP_EXT_TRACE_TYPE, 0},
+    {GTP_EXT_TRACE_REF, 2},
+    {GTP_EXT_TRACE_TYPE, 2},
     {GTP_EXT_USER_ADDR, 4},
     {GTP_EXT_APN, 0},
     {GTP_EXT_PROTO_CONF, 0},
-    {GTP_EXT_GSN_ADDR, 0},
     {GTP_EXT_GSN_ADDR, 0},
     {GTP_EXT_MSISDN, 0},
     {GTP_EXT_QOS_UMTS, 0},
@@ -55,10 +54,12 @@ bool pdp_stationid_match(uint64_t stationid, const struct xt_pdp_mtinfo *info)
 	return false;
     if (info->type == PDP_RESERVED)
 	for(i = 0; i < wl_stationid_size(); i++)
-	    if ((stationid >= wl_stationid[i].min) && (stationid <= wl_stationid[i].max))
+	    if ((stationid >= wl_stationid[i].min) && (stationid <= wl_stationid[i].max)) {
 		return true;
-    if ((info->type == PDP_STATION_ID) && (info->n == stationid))
+	    }
+    if ((info->type == PDP_STATION_ID) && (info->n == stationid)){
 	return true;
+    }
     return false;
 }
 
@@ -85,4 +86,18 @@ uint64_t msisdn_to_uint64(const uint8_t * ad, uint8_t len)
     return simple_strtoull(str, NULL, 10);
 }
 
+uint64_t imsi_to_uint64(const uint8_t * ad) {
+    char str[17] = "                ";
+    uint8_t i, j = 0;
+
+    for (i = 0; i < 8; i++) {
+        if ((ad[i] & 0x0F) <= 9)
+	    str[j++] = (ad[i] & 0x0F) + 0x30;
+	if (((ad[i] >> 4) & 0x0F) <= 9)
+	    str[j++] = ((ad[i] >> 4) & 0x0F) + 0x30;
+    }
+    str[j] = '\0';
+
+    return simple_strtoull(str, NULL, 10);
+}
 
